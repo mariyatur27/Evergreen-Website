274157be88b79e725e232f2406b38d6feafdc8e1
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-06-27 11:53:42 -0400

    adding the logo

--- the_evergreen_website/src/public/hero.js
+++ the_evergreen_website/src/public/hero.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from '../assets/black_logo.png'
 
 function Hero() {
   return (
    <div className='hero centered'>
-    {/* Update the text */}
+      <img src={logo} className='logo' alt='logo'/>
        <h1>Grow Your Wealth.</h1>
-       <h3>Evergreen Wealth Partners</h3>
+       {/* <h3>Evergreen Wealth Partners</h3> */}
        <a href = '/#about-section'><button className='hero-btn'>Learn More</button></a><br></br>
        <a href = ''><button className='hero-btn bottom-space'>Invest</button></a>
    </div>
